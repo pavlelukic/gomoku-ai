684d3dca3ac3d282b4bf9f5708fb8d283fb0978c
pavlelukic <pavlelukic2015@gmail.com>
2025-08-28 23:21:07 +0200

    Added place piece mechanic, which returns a board with a new piece placed at the desired place (if valid).

--- src/gomoku_ai/board.cljs
+++ src/gomoku_ai/board.cljs
@@ -7,3 +7,11 @@
   "Creates a 15x15 board filled with 0s."
   []
   (vec (repeat board-rows (vec (repeat board-cols 0)))))
+
+(defn place-piece
+  "Places a player's piece on the board at a specific [row col].
+   Returns the new updated board if the move is valid, otherwise nil."
+  [board [row col] player]
+  (if (= 0 (get-in board [row col]))
+    (assoc-in board [row col] player)
+    board))
